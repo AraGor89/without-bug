b1604465ac451ad7db97983eaa22c37a86758101
GorAraqelyan <50023373+GorAraqelyan@users.noreply.github.com>
2020-08-21 22:17:38 +0400

    Updates

---
+++ precache-manifest.197b15fc141ca8efa50160b30839647a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b208ab7c2d17aada18115244e6099f24",
+    "url": "/without-bug/index.html"
+  },
+  {
+    "revision": "bb2a24b6763ce83db4f9",
+    "url": "/without-bug/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "122cd866320a4971acd7",
+    "url": "/without-bug/static/js/2.56bac49b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/without-bug/static/js/2.56bac49b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb2a24b6763ce83db4f9",
+    "url": "/without-bug/static/js/main.ab4b2ce6.chunk.js"
+  },
+  {
+    "revision": "8e75eba49fed4c40af08",
+    "url": "/without-bug/static/js/runtime-main.5ea84bc0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/without-bug/static/media/logo.5d5d9eef.svg"
+  }
+]);
